c0d4b80de5ef7f9d66bf00606fb71fd71714e4b5
chritania <chritaniap@gmail.com>
2024-06-26 11:46:55 +0800

    Dashboard and Menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:untitled1/home.dart';
+import 'package:untitled1/pages/dashboard.dart';
+import 'package:untitled1/pages/menu.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+routes: {
+  '/' : (context)=> Dashboard(),
+  '/menu' : (context) => Menu(),
+  '/profile' : (context) => Home(),
+},
 ));
 
 
