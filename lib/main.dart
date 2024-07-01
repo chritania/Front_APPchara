a30aa42ed783bb9dd85ee7444d21ff0ee33daf0a
chritania <chritaniap@gmail.com>
2024-07-01 11:38:48 +0800

    Sign up with validation

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:untitled1/home.dart';
 import 'package:untitled1/pages/dashboard.dart';
 import 'package:untitled1/pages/menu.dart';
+import 'package:untitled1/pages/signup.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute : '/menu',
+  initialRoute : '/signup',
 routes: {
   '/' : (context)=> Dashboard(),
   '/menu' : (context) => Menu(),
   '/profile' : (context) => Home(),
+  '/signup' : (context) => Signup(),
 },
 ));
 
